cdc81ca48ec972945f0c8a8692abec3c2e9eea65
Tzachi Elrom <Tzachi.elrom94@gmail.com>
2020-05-13 01:39:34 +0300

    Updates

---
+++ precache-manifest.059b69608857857835fe82152267b23e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d013f4cbb7f731ccda4c146c505087",
+    "url": "/Full_Stack_Projects/index.html"
+  },
+  {
+    "revision": "ae47caa4a535c5dc2ddd",
+    "url": "/Full_Stack_Projects/static/css/main.5906fe1b.chunk.css"
+  },
+  {
+    "revision": "4ba5b832e4d9491589a3",
+    "url": "/Full_Stack_Projects/static/js/2.09d24e21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Full_Stack_Projects/static/js/2.09d24e21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae47caa4a535c5dc2ddd",
+    "url": "/Full_Stack_Projects/static/js/main.da203fa1.chunk.js"
+  },
+  {
+    "revision": "9e4fdbec7265e877af49",
+    "url": "/Full_Stack_Projects/static/js/runtime-main.d2337847.js"
+  }
+]);
